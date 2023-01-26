ce5e5a108f83731411be35b612e3952bc90cd2ff
Mandy <amanda.everett.works@gmail.com>
2023-01-26 15:46:28 -0700

    Feature/stateless (#7) (#8)
    * remove cookie requirement
    
    * set up stateless api call for different domains
    
    * new api url

--- src/common/utils/AppAxios.ts
+++ src/common/utils/AppAxios.ts
@@ -1,5 +1,4 @@
-import axios, { AxiosRequestConfig } from "axios";
-import Cookies from "js-cookie";
+import axios from "axios";
 
 const AppAxios = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
@@ -10,27 +9,5 @@ const AppAxios = axios.create({
   withCredentials: true,
   timeout: 10000,
 });
-
-const interceptMethods = ['post', 'put', 'delete'];
-const isInterceptMethod = (method: string | undefined) => interceptMethods.includes(method ?? "");
-
-const onRequest = async (config: AxiosRequestConfig): Promise<AxiosRequestConfig<any>> => {
-    const csrf = Cookies.get('XSRF-TOKEN');
-  if (csrf) {
-    return config;
-  }
-  if (isInterceptMethod(config.method)) {
-    await setCSRFToken();
-    return config;
-  }
-  return config;
-}
-
-const setCSRFToken = (): Promise<any> => {
-  return AppAxios.get('/sanctum/csrf-cookie'); 
-}
-
-AppAxios.interceptors.request.use(onRequest, null);
-
 
 export default AppAxios;
